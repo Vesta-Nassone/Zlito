b185598b4330e46f7037210f9e746d87ec0f704e
Vesta <vestanassone@gmail.com>
2020-01-20 01:49:04 +0200

    Initial commit. just the skeleton.

---
+++ app/src/main/java/com/example/zlito/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.zlito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize widgets
+        Button loginButton = findViewById(R.id.loginBtn);
+        //EditText userEmail = findViewById(R.id.userEmail);
+        //TextView forgotPassword = findViewById(R.id.forgotPasswordTextView);
+        //EditText password = findViewById(R.id.password);
+
+        //Calculator Button Listener
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                //passing info into the new Activity
+                startActivity(loginIntent);
+            }
+        });
+
+    }
+}
